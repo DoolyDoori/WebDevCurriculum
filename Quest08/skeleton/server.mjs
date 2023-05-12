f191e2c9f01b4d5aa701a2461729bc2dbd246dc7
DoolyDoori <leedoori11011@gmail.com>
2023-05-13 01:48:19 +0900

    [ADD] Quest8 Done

--- Quest08/skeleton/server.mjs
+++ Quest08/skeleton/server.mjs
@@ -1,8 +1,29 @@
 import http from 'http';
+import url from 'url';
 
 const server = http.createServer((req, res) => {
-    /* TODO: 각각의 URL들을 어떻게 처리하면 좋을까요? */
-    res.end();
+  const { pathname } = url.parse(req.url, true);
+  if (req.method === 'POST' && pathname === '/foo') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      try {
+        const data = JSON.parse(body);
+        if (data && data.bar) {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(`Hello, ${data.bar}`);
+        }
+      } catch (err) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Invalid JSON format');
+      }
+    });
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!');
+  }
 });
 
-server.listen(8000);
+server.listen(8080);
